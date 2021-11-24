0062c4baee5bc762d4f3fd56a3211482372d2e4d
Lina <owner@Owners-MacBook-Air.local>
2021-11-24 17:54:08 +0200

    commit

---
+++ main.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+  $(".category").hover(function(){
+    var desc = $(this).find(".description")
+    desc.fadeIn();
+    desc.animate({top: '650px'});
+  }, function() {
+    var desc = $(this).find(".description")
+    desc.animate({top: '900px'});
+  });
+});
+
+$(document).ready(function(){
+  $(".category").hover(function(){
+    var desc = $(this).find(".subtext")
+    desc.fadeIn();
+    desc.animate({top: '550px'});
+  }, function() {
+    var desc = $(this).find(".subtext")
+    desc.animate({top: '700px'});
+  });
+});
+
+const container = document.querySelector(".main");
+console.log(container)
+
+document.addEventListener("wheel", event => {
+  event.preventDefault();  
+  const container = document.querySelector(".main");
+  container.scrollLeft += event.deltaY;
+});
+
+// document.addEventListener("scroll", evt => {
+//   console.log(evt);
+// });
